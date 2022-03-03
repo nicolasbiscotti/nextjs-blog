28de411c0e09db446150afa55b3b68549b581ba1
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-03-03 20:15:36 -0300

    fix(pages/index.js): a tag under Link

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Read {' '}
-          <Link href="/posts/first-post">this page!</Link>
+          Read{" "}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
